342f19067305b7213c480bdfd2aa983e37ecef04
Jake Weinstein <xboxlover360@gmail.com>
2013-12-17 15:43:36 -0600

    vendor: add QCOM dalvik for QCOM devices
    Thanks to krzaq for the zip of the QCOM dalvik files
    
    Thanks to ctfrommn and MrBaNkS for the commit on PSX
    
    All I did was modify the paths for Vanir and ifneqs so all QCOM boards
    use this rather than having to specify each device
    
    Change-Id: I0f87b85d5db5d87950065d6c24a7b9b0311ad565
    
    Conflicts:
    	products/common.mk

--- products/common.mk
+++ products/common.mk
@@ -153,3 +153,16 @@ PRODUCT_COPY_FILES += \
 # Camera shutter sound property
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.camera-sound=1 
+
+# Qualcomm Optimized Dalvik
+QCOM_PATH:= vendor/romfactory/proprietary/qcom
+
+ifneq ($(filter msm8610 msm8x26 msm8226 msm8x74 msm8974 msm8960 msm8660 msm7627a msm7630_surf apq8084 mpq8092,$(TARGET_BOARD_PLATFORM)),)
+
+PRODUCT_COPY_FILES += \
+         $(QCOM_PATH)/bin/dalvikvm:system/bin/dalvikvm \
+         $(QCOM_PATH)/bin/dexopt:system/bin/dexopt \
+         $(QCOM_PATH)/lib/libcutils.so:system/lib/libcutils.so \
+         $(QCOM_PATH)/lib/libdvm.so:system/lib/libdvm.so \
+         $(QCOM_PATH)/lib/libqc-opt.so:system/lib/libqc-opt.so
+endif
